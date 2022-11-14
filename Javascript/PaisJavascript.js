8d5750612584ad1ff5411bf92581295ef3482772
Luis Velasco <luiselsw27@gmail.com>
2022-11-14 14:42:42 -0500

    update 1

--- Javascript/PaisJavascript.js
+++ Javascript/PaisJavascript.js
@@ -47,7 +47,7 @@ const retornarPaisNombre=async()=>{
         const fronteras=data.borders;
         let fronterasHtml=''
         for (const key in fronteras) {
-            fronterasHtml+=`<a href='#'><span class='border' data-id='${fronteras[key]}'>${fronteras[key].toLowerCase()}</span></a>`
+            fronterasHtml+=`<a href='../Info_pais/pais.html'><span class='border' data-id='${fronteras[key]}'>${fronteras[key].toLowerCase()}</span></a>`
         }
 
         const nombreNativo=data.name.nativeName;
